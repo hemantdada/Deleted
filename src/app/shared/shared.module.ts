652bd2ec19111d97027dec930ff1171fc5bd284e
hemantdada <hemantpatil4@gmail.com>
2019-06-23 22:43:54 +0530

    save

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,7 +1,7 @@
 
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
 import { ModuleWithProviders, NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
